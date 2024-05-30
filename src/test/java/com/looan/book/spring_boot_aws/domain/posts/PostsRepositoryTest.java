77ba8b736b33c7cd92d2f2fbe290464f05b48cd6
LooanCheong <jjyejun@naver.com>
2024-05-30 19:15:45 +0900

    Test: 게시글 생성 및 저장 테스트

--- src/test/java/com/looan/book/spring_boot_aws/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/looan/book/spring_boot_aws/domain/posts/PostsRepositoryTest.java
@@ -1,4 +1,44 @@
+package com.looan.book.spring_boot_aws.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class PostsRepositoryTest {
-  
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    void cleanUp() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    void 게시글저장_불러오기() {
+        String title = "게시글 테스트";
+        String content = "게시글 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("test@gmail.com")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
 }
