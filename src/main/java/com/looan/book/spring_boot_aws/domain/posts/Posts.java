6ab45e0ae73ab3d57adb32dd2a25fece0a0128f9
LooanCheong <jjyejun@naver.com>
2024-05-31 07:36:27 +0900

    Feat: BaseTimeEntity 상속받게 변경

--- src/main/java/com/looan/book/spring_boot_aws/domain/posts/Posts.java
+++ src/main/java/com/looan/book/spring_boot_aws/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.looan.book.spring_boot_aws.domain.posts;
 
+import com.looan.book.spring_boot_aws.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
