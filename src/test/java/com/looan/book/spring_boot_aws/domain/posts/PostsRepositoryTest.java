526fdcd727c89962f435e146bd48862025a9eebf
LooanCheong <jjyejun@naver.com>
2024-05-31 07:39:43 +0900

    Test: 게시글 등록, 삭제 시간 등록 테스트

--- src/test/java/com/looan/book/spring_boot_aws/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/looan/book/spring_boot_aws/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -39,6 +40,26 @@ class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    void BaseTimeEntity_등록() {
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>> createDate = " + posts.getCreatedDate());
+        System.out.println(">>>>>>>>>>> modifiedDate = " + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
 }
