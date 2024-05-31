4ef8a0744c056c2ef8bb9e6d277137b2c3592f54
LooanCheong <jjyejun@naver.com>
2024-05-31 09:09:15 +0900

    Feat: 게시글 내림차순 쿼리 추가

--- src/main/java/com/looan/book/spring_boot_aws/domain/posts/PostsRepository.java
+++ src/main/java/com/looan/book/spring_boot_aws/domain/posts/PostsRepository.java
@@ -1,6 +1,12 @@
 package com.looan.book.spring_boot_aws.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("select p from Posts p order by p.id desc")
+    List<Posts> findAllDesc();
 }
