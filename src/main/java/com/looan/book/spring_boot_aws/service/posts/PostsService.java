ded4470cce34fc21215166768c4a1f63c6fd1c68
LooanCheong <jjyejun@naver.com>
2024-05-31 09:13:11 +0900

    Feat: 게시글 리스트 내림차순 반환용 서비스 추가

--- src/main/java/com/looan/book/spring_boot_aws/service/posts/PostsService.java
+++ src/main/java/com/looan/book/spring_boot_aws/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.looan.book.spring_boot_aws.service.posts;
 
 import com.looan.book.spring_boot_aws.domain.posts.Posts;
 import com.looan.book.spring_boot_aws.domain.posts.PostsRepository;
+import com.looan.book.spring_boot_aws.web.dto.PostsListResponseDto;
 import com.looan.book.spring_boot_aws.web.dto.PostsResponseDto;
 import com.looan.book.spring_boot_aws.web.dto.PostsSaveRequestDto;
 import com.looan.book.spring_boot_aws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
