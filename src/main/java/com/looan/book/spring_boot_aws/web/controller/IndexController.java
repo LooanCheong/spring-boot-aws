1eb878e1bbc77d19e02f4a0cfc76fc9fb6542f49
LooanCheong <jjyejun@naver.com>
2024-06-05 00:14:54 +0900

    Feat: 유저 세션 정보 추가 기능 구현

--- src/main/java/com/looan/book/spring_boot_aws/web/controller/IndexController.java
+++ src/main/java/com/looan/book/spring_boot_aws/web/controller/IndexController.java
@@ -1,7 +1,9 @@
 package com.looan.book.spring_boot_aws.web.controller;
 
+import com.looan.book.spring_boot_aws.config.auth.dto.SessionUser;
 import com.looan.book.spring_boot_aws.service.posts.PostsService;
 import com.looan.book.spring_boot_aws.web.dto.PostsResponseDto;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +15,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession
+                .getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
