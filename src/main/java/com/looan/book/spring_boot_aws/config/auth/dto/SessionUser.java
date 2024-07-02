92d78b81a90febc8361b00eb86c7874f832cdd7d
LooanCheong <jjyejun@naver.com>
2024-07-02 11:27:56 +0900

    Feat: 세션 유저 직렬화 implements 추가

--- src/main/java/com/looan/book/spring_boot_aws/config/auth/dto/SessionUser.java
+++ src/main/java/com/looan/book/spring_boot_aws/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.looan.book.spring_boot_aws.config.auth.dto;
 import com.looan.book.spring_boot_aws.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
     private String name;
     private String email;
